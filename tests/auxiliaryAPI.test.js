aa60676470b67de4f63ab3e83e1f34e4a333eba8
Jannis Vollmer <jannis.vollmer@edu.umit-tirol.at>
2023-11-05 16:35:01 +0100

    added tests for two transform methods

--- tests/auxiliaryAPI.test.js
+++ tests/auxiliaryAPI.test.js
@@ -14,3 +14,18 @@ test('Umwandlung von Celsius in Fahrenheit (-40 Grad Celsius)', () => {
   const result = auxiliaryAPI.celsiusToFahrenheit(-40);
   expect(result).toBe(-40);
 });
+
+test('Fahrenheit in Celsius umwandeln (32 Grad Fahrenheit)', () => {
+  const result = auxiliaryAPI.fahrenheitToCelcius(32);
+  expect(result).toBe(0); // 32 °F entsprechen 0 °C
+});
+
+test('Fahrenheit in Celsius umwandeln (100 Grad Fahrenheit)', () => {
+  const result = auxiliaryAPI.fahrenheitToCelcius(100);
+  expect(result).toBe(37.77777777777778); // 100 °F entsprechen etwa 37.78 °C (gerundet)
+});
+
+test('Fahrenheit in Celsius umwandeln (-40 Grad Fahrenheit)', () => {
+  const result = auxiliaryAPI.fahrenheitToCelcius(-40);
+  expect(result).toBe(-40); // -40 °F entsprechen -40 °C
+});
