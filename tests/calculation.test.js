40b11af23988c39a352d2b5ffff2eceb0f5b0876
Jannis Vollmer <jannis.vollmer@edu.umit-tirol.at>
2023-11-05 17:37:11 +0100

    added extension to jest

--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -1,42 +1,35 @@
-
 const calculation = require('../public/js/calculation');
 
-// Mock für DOM-Elemente
-let powerButton;
-let temperatureObj;
-let levelObj;
-let modusObj;
-
-beforeEach(() => {
-  // Erstellen Sie DOM-Elemente für Ihre Tests
-  document.body.innerHTML = `
-    <button id="on-off-button">Power</button>
-    <p id="temperature"></p>
-    <p id="level"></p>
-    <p id="modus"></p>
-  `;
-
-  powerButton = document.getElementById('on-off-button');
-  temperatureObj = document.getElementById('temperature');
-  levelObj = document.getElementById('level');
-  modusObj = document.getElementById('modus');
-});
-
 describe('Unit-Tests für Funktionen ohne DOM und Benutzerinteraktion', () => {
+  let initialTemperature;
+  let initialMode;
+  let initialLevel;
+
+  beforeAll(() => {
+    // Speichern Sie die Anfangswerte, um den Status der Funktionen zu überwachen
+    initialTemperature = calculation.temperature;
+    initialMode = calculation.mode;
+    initialLevel = calculation.level;
+  });
+
+  beforeEach(() => {
+    // Setzen Sie den Status auf die Anfangswerte zurück
+    calculation.temperature = initialTemperature;
+    calculation.mode = initialMode;
+    calculation.level = initialLevel;
+  });
+
   test('decreaseTemperature verringert die Temperatur um 1 Grad', () => {
-    const initialTemperature = calculation.temperature;
     calculation.decreaseTemperature();
     expect(calculation.temperature).toBe(initialTemperature - 1);
   });
 
   test('increaseTemperature erhöht die Temperatur um 1 Grad', () => {
-    const initialTemperature = calculation.temperature;
     calculation.increaseTemperature();
     expect(calculation.temperature).toBe(initialTemperature + 1);
   });
 
   test('levelChange ändert das Level der Klimaanlage', () => {
-    const initialLevel = calculation.level;
     calculation.levelChange();
     if (initialLevel < 3) {
       expect(calculation.level).toBe(initialLevel + 1);
@@ -46,7 +39,6 @@ describe('Unit-Tests für Funktionen ohne DOM und Benutzerinteraktion', () => {
   });
 
   test('modeChange ändert den Modus', () => {
-    const initialMode = calculation.mode;
     calculation.modeChange();
     if (initialMode === 'summer') {
       expect(calculation.mode).toBe('winter');
