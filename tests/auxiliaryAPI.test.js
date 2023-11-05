4615bc41f0816646b4b4c40568aff5fa602d2e8c
Jannis Vollmer <jannis.vollmer@edu.umit-tirol.at>
2023-11-05 17:08:49 +0100

    added new tests (calculation.js and rest for auxiliaryAPI.js)

--- tests/auxiliaryAPI.test.js
+++ tests/auxiliaryAPI.test.js
@@ -1,31 +1,87 @@
+// Dieses Testskript nutzt Jest als Framework zum Testen
 const auxiliaryAPI = require('../public/js/auxiliaryAPI');
 
+// Tests für die Umwandlung von Celsius nach Fahrenheit
+// Input: 0 Grad Celsius
+// erwarteter Output: 32 Grad Fahrenheit
 test('Umwandlung von Celsius in Fahrenheit (0 Grad Celsius)', () => {
   const result = auxiliaryAPI.celsiusToFahrenheit(0);
   expect(result).toBe(32);
 });
 
+// Input: 100 Grad Celsius
+// erwarteter Output: 212 Grad Fahrenheit
 test('Umwandlung von Celsius in Fahrenheit (100 Grad Celsius)', () => {
   const result = auxiliaryAPI.celsiusToFahrenheit(100);
   expect(result).toBe(212);
 });
 
+// Input: -40 Grad Celsius
+// erwarteter Output: -40 Grad Fahrenheit
 test('Umwandlung von Celsius in Fahrenheit (-40 Grad Celsius)', () => {
   const result = auxiliaryAPI.celsiusToFahrenheit(-40);
   expect(result).toBe(-40);
 });
 
+// Tests für die Umwandlung von Fahrenheit nach Celsius
+// Input: 32 Grad Fahrenheit
+// erwarteter Output: 0 Grad Celsius
 test('Fahrenheit in Celsius umwandeln (32 Grad Fahrenheit)', () => {
   const result = auxiliaryAPI.fahrenheitToCelcius(32);
-  expect(result).toBe(0); // 32 °F entsprechen 0 °C
+  expect(result).toBe(0);
 });
 
+// Input: 100 Grad Fahrenheit
+// erwarteter Output: 37.78 Grad Celsius
 test('Fahrenheit in Celsius umwandeln (100 Grad Fahrenheit)', () => {
   const result = auxiliaryAPI.fahrenheitToCelcius(100);
-  expect(result).toBe(37.77777777777778); // 100 °F entsprechen etwa 37.78 °C (gerundet)
+  expect(result).toBe(37.77777777777778);
 });
 
+// Input: -40 Grad Fahrenheit
+// erwarteter Output: -40 Grad Celsius
 test('Fahrenheit in Celsius umwandeln (-40 Grad Fahrenheit)', () => {
   const result = auxiliaryAPI.fahrenheitToCelcius(-40);
-  expect(result).toBe(-40); // -40 °F entsprechen -40 °C
+  expect(result).toBe(-40);
+});
+
+
+// Mock für den aktuellen Zeitpunkt (myDate)
+const mockDate = new Date('2022-01-01 08:00'); // beispielhaftes setzen der Uhrzeit auf 8:00 Uhr morgens
+
+describe('getGreetingDependOnTime', () => {
+  beforeAll(() => {
+    // Vor allen Tests die Date-Funktion mocken
+    global.Date = class extends Date {
+      constructor() {
+        super();
+        return mockDate;
+      }
+    };
+  });
+
+  afterAll(() => {
+    // Nach allen Tests die Date-Funktion wiederherstellen
+    global.Date = Date;
+  });
+
+  test('Begrüßung am Morgen (zwischen 6 und 21 Uhr)', () => {
+    // Uhrzeit ist hier auf 8 Uhr gestellt (siehe oben)
+    const result = auxiliaryAPI.getGreetingDependOnTime(mockDate);
+    expect(result).toBe('Guten Morgen');
+  });
+
+  test('Begrüßung am Abend (nach 21 Uhr)', () => {
+    // Aktualisieren der mockDate auf eine Uhrzeit nach 21 Uhr
+    mockDate.setHours(22);
+    const result = auxiliaryAPI.getGreetingDependOnTime(mockDate);
+    expect(result).toBe('Guten Abend');
+  });
+
+  test('Begrüßung am Abend (vor 6 Uhr)', () => {
+    // Aktualisieren der mockDate auf eine Uhrzeit vor 6 Uhr
+    mockDate.setHours(5);
+    const result = auxiliaryAPI.getGreetingDependOnTime(mockDate);
+    expect(result).toBe('Guten Abend');
+  });
 });
---
+++ tests/calculation.test.js
@@ -0,0 +1,46 @@
+const calculation = require('../public/js/auxiliaryAPI');
+
+// Mock für globale Variablen
+let isOn = true;
+let temperature = 25;
+let level = 2;
+let mode = 'summer';
+
+describe('Funktionen für die Steuerung', () => {
+
+  test('decreaseTemperature verringert die Temperatur um 1 Grad', () => {
+    const initialTemperature = temperature;
+    decreaseTemperature();
+    expect(temperature).toBe(initialTemperature - 1);
+    // Erwartetes Verhalten: Die Temperatur sollte um 1 Grad verringert werden.
+  });
+
+  test('increaseTemperature erhöht die Temperatur um 1 Grad', () => {
+    const initialTemperature = temperature;
+    increaseTemperature();
+    expect(temperature).toBe(initialTemperature + 1);
+    // Erwartetes Verhalten: Die Temperatur sollte um 1 Grad erhöht werden.
+  });
+
+  test('levelChange ändert das Lüfterlevel', () => {
+    const initialLevel = level;
+    levelChange();
+    if (initialLevel < 3) {
+      expect(level).toBe(initialLevel + 1);
+    } else {
+      expect(level).toBe(0);
+    }
+    // Erwartetes Verhalten: Das Level sollte geändert werden (von 0 bis 3).
+  });
+
+  test('modeChange ändert den Modus', () => {
+    const initialMode = mode;
+    modeChange();
+    if (initialMode === 'summer') {
+      expect(mode).toBe('winter');
+    } else {
+      expect(mode).toBe('summer');
+    }
+    // Erwartetes Verhalten: Der Modus sollte geändert werden (von "summer" zu "winter" oder umgekehrt).
+  });
+});
